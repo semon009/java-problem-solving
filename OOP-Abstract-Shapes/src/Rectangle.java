6ff42434dca02de0aa39898c734778c0113f4243
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-03 17:20:48 +0300

    Add Rectangle class extending Shape with area method

---
+++ OOP-Abstract-Shapes/src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle extends Shape {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
