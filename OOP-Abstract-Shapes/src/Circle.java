57622e4f13be0ab9439a87c080f9cd07e89226a7
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-03 17:21:01 +0300

    Add Circle class to calculate area

---
+++ OOP-Abstract-Shapes/src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
