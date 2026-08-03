8f481e15cb2571aff8d94f870e4cb52746e11428
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-03 19:54:58 +0300

    Implement star counter in Main class

---
+++ java-secret-star-counter/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        for (Secret secret : Secret.values()) {
+            if (secret.name().startsWith("STAR")) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+}
