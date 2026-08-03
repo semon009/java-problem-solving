a3448c8633322f29f752a9029b8ae84f6a1c541c
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-03 16:55:15 +0300

    Create BaseEntity class with id and version fields

---
+++ OOP-Inheritance-Practice/src/BaseEntity.java
@@ -0,0 +1,12 @@
+public abstract class BaseEntity {
+    protected long id;
+    protected long version;
+
+    public long getId() {return id;}
+  
+    public void setId(long id) {this.id = id;}
+  
+    public long getVersion() {return version;}
+  
+    public void setVersion(long version) {this.version = version;}
+}
