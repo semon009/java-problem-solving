57079ee2e8f7985d47ca06017688b2b051776a10
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-04 13:18:35 +0300

    Create SI enum for standard SI units

---
+++ java-enums-si-units/SI.java
@@ -0,0 +1,12 @@
+public enum SI {
+
+    M("length"),
+    KG("mass"),
+    S("time");
+
+    public final String quantityName;
+
+    SI(String quantityName) {this.quantityName = quantityName;}
+
+    public String getQuantityName() {return quantityName;}
+}
